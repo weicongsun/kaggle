f9257f186bfa9246b029dfe75e42c3da2e7dee12
weicongsun <sunweicong85@gmail.com>
2013-10-30 19:49:02 +0800

    dog&cat: loaddata

--- dog&cat/dog_cat.py
+++ dog&cat/dog_cat.py
@@ -1,23 +1,27 @@
 from scipy.misc  import imread, imresize
 import numpy as np 
 
-def load_imgs(filepath='./tmp/', slices):
-	data=[]
-	target=[]
-	for f in os.listdir(filepath):
+def load_imgs(folder_path='./tmp/', slices):
+	filepaths = [join(folder_path, f) for f in listdir(folder_path)]
+	n_imgs = len(filepaths)
+	data = np.zeros((n_imgs, 64*64)
+	target = np.zeros(n_imgs)
+	for i, f in enumerate(filepaths):
 		print f
-		img = np.asarray(imread(filepath+f), dtype=np.float32)
+		img = np.asarray(imread(f), dtype=np.float32)
 		img = imresize(img, (64,64))	
 		img = img.mean(2)
 		img /= 255.0
-		data.append(img)
+		data[i, ...] = img
 
 		if f.startswith('cat') :
-			target.append(0)
+			target[i] = 0
 		else:
-			target.append(1)
+			target[i] = 1
 
-	indices = np.arrange(data)	
+
+
+	indices = np.arrange(n_imgs)	
 	np.random.RandomState(42).shuffle(indices)
 	data, target = data[indices], target[indices]
 	return data, target
